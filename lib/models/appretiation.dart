e59818e47b3b1ab794fd24dd2b460fd6e44bebad
golibjon <golibtoramurodov@gmail.com>
2024-07-08 09:46:59 +0500

    me

---
+++ lib/models/appretiation.dart
@@ -0,0 +1,10 @@
+class Appretiation {
+  String awardName;
+  DateTime endDate;
+  String appretiationDesc;
+
+  Appretiation(
+      {required this.awardName,
+      required this.endDate,
+      required this.appretiationDesc});
+}
---
+++ lib/models/education.dart
@@ -0,0 +1,18 @@
+class Education {
+  String levelOfEducation;
+  String institutionName;
+  String fieldOfStudy;
+  DateTime startDate;
+  DateTime endDate;
+  bool currentlyStudying;
+  String studyDesc;
+
+  Education(
+      {required this.levelOfEducation,
+      required this.institutionName,
+      required this.fieldOfStudy,
+      required this.startDate,
+      required this.endDate,
+      required this.currentlyStudying,
+      required this.studyDesc});
+}
