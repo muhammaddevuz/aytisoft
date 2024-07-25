89740c28d866fe7f12600b3ba9bcaa1da54e994e
Jamshidmirzo <101593441+Jamshidmirzo@users.noreply.github.com>
2024-07-25 10:19:59 +0500

    done experinece

--- lib/ui/screens/workExp_screen.dart
+++ lib/ui/screens/workExp_screen.dart
@@ -11,7 +11,16 @@ class WorkexpScreen extends StatefulWidget {
 }
 
 class _WorkexpScreenState extends State<WorkexpScreen> {
-  submit() {}
+  final _formKey = GlobalKey<FormState>();
+
+  void submit() {
+    if (_formKey.currentState!.validate()) {
+      // Process the data if the form is valid
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,173 +28,176 @@ class _WorkexpScreenState extends State<WorkexpScreen> {
       appBar: AppBar(),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Add work experience',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Color(0xFF150B3D),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Add work experience',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color(0xFF150B3D),
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'Job title',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Color(0xFF150B3D),
+              const SizedBox(height: 30),
+              const Text(
+                'Job title',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color(0xFF150B3D),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Textfieldform(
-              filledColor: Colors.white,
-              validatorfunc: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter job title';
-                }
-                return null;
-              },
-              labelText: 'Title',
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Company',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Color(0xFF150B3D),
+              const SizedBox(height: 10),
+              Textfieldform(
+                filledColor: Colors.white,
+                validatorfunc: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter job title';
+                  }
+                  return null;
+                },
+                labelText: 'Title',
               ),
-            ),
-            const SizedBox(height: 10),
-            Textfieldform(
-              filledColor: Colors.white,
-              validatorfunc: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter company name';
-                }
-                return null;
-              },
-              labelText: 'Company',
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Start Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Color(0xFF150B3D),
+              const SizedBox(height: 10),
+              const Text(
+                'Company',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color(0xFF150B3D),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Textfieldform(
+                filledColor: Colors.white,
+                validatorfunc: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter company name';
+                  }
+                  return null;
+                },
+                labelText: 'Company',
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Start Date',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Color(0xFF150B3D),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      Textfieldform(
-                        filledColor: Colors.white,
-                        validatorfunc: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter start date';
-                          }
-                          return null;
-                        },
-                        labelText: 'Start Date',
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'End Date',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Color(0xFF150B3D),
+                        const SizedBox(height: 10),
+                        Textfieldform(
+                          filledColor: Colors.white,
+                          validatorfunc: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter start date';
+                            }
+                            return null;
+                          },
+                          labelText: 'Start Date',
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      Textfieldform(
-                        filledColor: Colors.white,
-                        validatorfunc: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter end date';
-                          }
-                          return null;
-                        },
-                        labelText: 'End Date',
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: false,
-                  onChanged: (value) {
-                    value = false;
-                  },
-                ),
-                const Text(
-                  'This is my position now',
-                ),
-              ],
-            ),
-            const Text(
-              'Description',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-                color: Color(0xFF150B3D),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'End Date',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Color(0xFF150B3D),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Textfieldform(
+                          filledColor: Colors.white,
+                          validatorfunc: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter end date';
+                            }
+                            return null;
+                          },
+                          labelText: 'End Date',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 10),
-            Textfieldform(
-              maxLines: 5,
-              filledColor: Colors.white,
-              validatorfunc: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter description name';
-                }
-                return null;
-              },
-              labelText: 'Write additional information here',
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: ZoomTapAnimation(
-                onTap: submit,
-                child: Card(
-                  color: const Color(0xFF130160),
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: 270,
-                    height: 70,
-                    child: const Text(
-                      "Save",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
+              Row(
+                children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: (value) {
+                      setState(() {
+                        // Handle checkbox state
+                      });
+                    },
+                  ),
+                  const Text(
+                    'This is my position now',
+                  ),
+                ],
+              ),
+              const Text(
+                'Description',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color(0xFF150B3D),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Textfieldform(
+                maxLines: 5,
+                filledColor: Colors.white,
+                validatorfunc: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter description';
+                  }
+                  return null;
+                },
+                labelText: 'Write additional information here',
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ZoomTapAnimation(
+                  onTap: submit,
+                  child: Card(
+                    color: const Color(0xFF130160),
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 270,
+                      height: 70,
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
