61ea88cc25c8599f10f54ad8edf3a0b4d48d8da7
Jamshidmirzo <101593441+Jamshidmirzo@users.noreply.github.com>
2024-07-25 16:44:24 +0500

    smth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:aytijobs/ui/screens/workExp_screen.dart';
+import 'package:aytijobs/ui/screens/testscreen.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WorkexpScreen(),
+      home: Testscreen(),
     );
   }
 }
