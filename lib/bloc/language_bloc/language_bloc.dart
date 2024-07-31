0f2016097bcee36bf5b1d70a53733418ab08096c
Muhammad <149857879+muhammaddevuz@users.noreply.github.com>
2024-07-31 09:25:11 +0500

    skills bloc

---
+++ lib/bloc/language_bloc/language_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+
+import 'language_event.dart';
+import 'language_state.dart';
+import 'package:aytijobs/data/models/language.dart';
+
+class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
+  LanguageBloc() : super(LanguageInitial()) {
+    on<LoadLanguages>(_onLoadLanguages);
+    on<SaveLanguages>(_onSaveLanguages);
+  }
+
+  void _onLoadLanguages(LoadLanguages event, Emitter<LanguageState> emit) {
+    // Bu yerda boshlang'ich tillarni yuklashni amalga oshirasiz.
+    List<Language> languages = []; // Tillarni yuklash
+    emit(LanguageLoadSuccess(languages));
+  }
+
+  void _onSaveLanguages(SaveLanguages event, Emitter<LanguageState> emit) {
+    // Bu yerda tillarni saqlash kodini qo'shing.
+    // Masalan, server yoki lokal ma'lumotlar bazasiga saqlash
+    emit(LanguageSaveSuccess());
+  }
+}
---
+++ lib/bloc/language_bloc/language_event.dart
@@ -0,0 +1,19 @@
+import 'package:aytijobs/data/models/language.dart';
+
+abstract class LanguageEvent {
+  const LanguageEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadLanguages extends LanguageEvent {}
+
+class SaveLanguages extends LanguageEvent {
+  final List<Language> languages;
+
+  const SaveLanguages(this.languages);
+
+  @override
+  List<Object> get props => [languages];
+}
