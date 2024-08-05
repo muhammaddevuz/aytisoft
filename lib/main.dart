3e13c435f1586024ec509ab410c1aba99a28dc42
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-08-05 08:33:23 +0500

    dadad

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:aytijobs/blocs/auth/auth_bloc.dart';
+import 'package:aytijobs/blocs/skills_bloc/skills_bloc.dart';
 import 'package:aytijobs/data/repositories/auth_user_repository.dart';
 import 'package:aytijobs/services/auth_firebase_service.dart';
 import 'package:aytijobs/firebase_options.dart';
 import 'package:aytijobs/services/get_it.dart';
+import 'package:aytijobs/ui/screens/profile_screen/add_skills_screen.dart';
 import 'package:aytijobs/ui/screens/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -23,14 +25,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider(
       create: (context) => AuthUserRepository(getIt.get<AuthFirebaseService>()),
-      child: BlocProvider(
-        create: (context) => AuthBloc(getIt.get<AuthUserRepository>()),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthBloc(getIt.get<AuthUserRepository>()),
+          ),
+          BlocProvider(
+            create: (context) => SkillsBloc(),
+          ),
+        ],
         child: ScreenUtilInit(
           designSize: const Size(360, 690),
           builder: (context, child) {
-            return const MaterialApp(
+            return MaterialApp(
               debugShowCheckedModeBanner: false,
-              home: SplashScreen(),
+              home: AddSkillsScreen(),
             );
           },
         ),
